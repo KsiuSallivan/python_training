ab29146f447df22d70013030fb6e97a698d6fb01
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-05 14:21:45 +0300

    Исправлена опечатка

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -22,7 +22,7 @@ def test_modify_contact_lastname(app):
         app.contact.data_contact(Contact(firstname="Kseniya", lastname="Kurashova", nickname="KsiuSallivan",
                                          email="ksiu.sallivan@gmail.com"))
         app.contact.submit_button()
-        app.pen_home_page()
+        app.open_home_page()
     app.contact.select_first_contact()
     app.contact.edit_button()
     app.contact.modify_first_contact(Contact(lastname="Twilight"))
