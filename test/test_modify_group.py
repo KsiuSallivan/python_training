f39bdb6852eb91de61ddb3149e4ff41469a37c12
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-11 12:55:46 +0300

    Сравнения в группах (с лекции)

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -9,25 +9,29 @@ def test_modify_group_name(app):
         app.group.submit_button()
         app.group.open_group_page()
     old_groups = app.group.get_group_list()
+    group = Group(name="New funny name")
+    group.id = old_groups[0].id
     app.group.select_first_group()
     app.group.edit_button()
-    app.group.modify_first_group(Group(name="New funny name"))
+    app.group.modify_first_group(group)
     app.group.update_button()
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_header(app):
-    app.group.open_group_page()
-    if app.group.count() == 0:
-        app.group.create_button()
-        app.group.data_group(Group(name="147", header="555", footer="665"))
-        app.group.submit_button()
-        app.group.open_group_page()
-    old_groups = app.group.get_group_list()
-    app.group.select_first_group()
-    app.group.edit_button()
-    app.group.modify_first_group(Group(header="New funny header"))
-    app.group.update_button()
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+# def test_modify_group_header(app):
+#     app.group.open_group_page()
+#     if app.group.count() == 0:
+#         app.group.create_button()
+#         app.group.data_group(Group(name="147", header="555", footer="665"))
+#         app.group.submit_button()
+#         app.group.open_group_page()
+#     old_groups = app.group.get_group_list()
+#     app.group.select_first_group()
+#     app.group.edit_button()
+#     app.group.modify_first_group(Group(header="New funny header"))
+#     app.group.update_button()
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
