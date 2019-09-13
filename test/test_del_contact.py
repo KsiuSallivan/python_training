d1838f5569cb686933890278bd30a716daaee0f6
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-13 14:02:19 +0300

    Сравнение контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -10,5 +10,10 @@ def test_delete_first_contact(app):
                                          email="ksiu.sallivan@gmail.com"))
         app.contact.submit_button()
         app.open_home_page()
+    old_contacts = app.contact.get_contact_list()
     app.contact.select_first_contact()
     app.contact.delete_button()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
