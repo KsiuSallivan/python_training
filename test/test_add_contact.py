8e16d48f8d53d35151cc1e66c035dc493a2d781f
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-13 18:12:50 +0300

    Сортировка списков перед сравнением для контактов (не работает удаление и модификация контактов)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,9 +4,9 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create_contact(Contact(firstname="Kseniya", lastname="Kurashova", nickname="KsiuSallivan",
-                                       email="ksiu.sallivan@gmail.com"))
+    contact = Contact(firstname="Kseniya", lastname="Kurashova", nickname="KsiuSallivan", email="ksiu.sallivan@gmail.com")
+    app.contact.create_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
-    # old_contacts.append(contact)
-    # assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -7,13 +7,13 @@ def test_modify_contact_firstname(app):
         app.contact.create_contact(Contact(firstname="Kseniya", lastname="Kurashova", nickname="KsiuSallivan",
                                            email="ksiu.sallivan@gmail.com"))
 
-    # contact = Contact(firstname="Candy")
-    # contact.id = old_contacts[0].id
-    app.contact.modify_first_contact(Contact(firstname="Candy"))
+    contact = Contact(firstname="Candy")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    # old_contacts[0] = contact
-    # assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 # def test_modify_contact_lastname(app):
