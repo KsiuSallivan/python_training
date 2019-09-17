63ec152da44d287b6b92128e15c2e4f10a0b0fc2
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-17 12:13:30 +0300

    Передача параметров в тесты из командной строки при запуске

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,21 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +32,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (len(wd.find_elements_by_xpath("//input[@value='Send e-Mail']")) > 0):
-            wd.get("http://localhost/addressbook/index.php")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
