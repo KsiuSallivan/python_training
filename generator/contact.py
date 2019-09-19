2d8bdf28cead88a40d3d33657d7ffd7d8af891e5
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-19 17:19:15 +0300

    Генератор данных для групп и контактов

---
+++ generator/contact.py
@@ -0,0 +1,47 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", nickname="", email="", homephone="", mobilephone="", workphone="",
+                    secondaryphone="", email2="", email3="", address="", )] + [
+    Contact(firstname=random_string("Kseniya", 10), lastname=random_string("Kurashova", 10),
+            nickname=random_string("Kotik", 10), email=random_string("kot@kot.kot", 10),
+            homephone=random_string("123", 10), mobilephone=random_string("456", 10),
+            workphone=random_string("789", 10), secondaryphone=random_string("147", 10),
+            email2=random_string("kot2@kot2.kot", 10), email3=random_string("kot3@kot3.kot", 10),
+            address=random_string("Gh12", 10))
+    for i in range(5)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- generator/group.py
+++ generator/group.py
@@ -25,15 +25,13 @@ for o, a in opts:
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + " "*10
     return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
 
 
-testdata = [
-        Group(name=name, header=header, footer=footer)
-        for name in ["", random_string("name", 10)]
-        for header in ["", random_string("header", 20)]
-        for footer in ["", random_string("footer", 20)]
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name1", 10), header=random_string("header1", 10), footer=random_string("footer1", 10))
+    for i in range(5)
 ]
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
