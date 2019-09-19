cfaf142f37194120f91d585f1392952246e7d5ee
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-19 16:06:42 +0300

    Реализовать проверки для всех полей контакта на главной странице - попытка 3

--- test/test_contacts_info.py
+++ test/test_contacts_info.py
@@ -3,7 +3,7 @@ import re
 
 def test_contact_info_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
