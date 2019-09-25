08789e25a1c52a261643140bd0a944ad0d4bcd97
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-25 13:07:36 +0300

    Фикстура для взаимодействия с базой данных - v2

--- conftest.py
+++ conftest.py
@@ -33,12 +33,13 @@ def app(request):
 @pytest.fixture(scope="session")
 def db(request):
     db_config = load_config(request.config.getoption("--target"))['db']
-    dbFixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'],
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'],
                           password=db_config['password'])
+    
     def fin():
-        dbFixture.destroy()
+        dbfixture.destroy()
     request.addfinalizer(fin)
-    return dbFixture
+    return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)
--- fixture/db.py
+++ fixture/db.py
@@ -13,32 +13,32 @@ class DbFixture:
         self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
         self.connection.autocommit = True
 
-    def get_group_list(self):
-        list = []
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
-            for row in cursor:
-                (id, name, header, footer) = row
-                list.append(Group(id=str(id), name=name, header=header, footer=footer))
-        finally:
-            cursor.close()
-        return list
-
-    def get_contact_list(self):
-        list = []
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("select id, firstname, lastname, nickname, address, home, mobile, work, email, email2,"
-                           " email3, phone2 from addressbook where deprecated='0000-00-00 00:00:00'")
-            for row in cursor:
-                (id, firstname, lastname, nickname, address, home, mobile, work, email, email2, email3, phone2) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, nickname=nickname,
-                                    address=address, homephone=home, mobilephone=mobile, workphone=work,
-                                    email=email, email2=email2, email3=email3, secondaryphone=phone2))
-        finally:
-            cursor.close()
-        return list
+    # def get_group_list(self):
+    #     list = []
+    #     cursor = self.connection.cursor()
+    #     try:
+    #         cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+    #         for row in cursor:
+    #             (id, name, header, footer) = row
+    #             list.append(Group(id=str(id), name=name, header=header, footer=footer))
+    #     finally:
+    #         cursor.close()
+    #     return list
+    #
+    # def get_contact_list(self):
+    #     list = []
+    #     cursor = self.connection.cursor()
+    #     try:
+    #         cursor.execute("select id, firstname, lastname, nickname, address, home, mobile, work, email, email2,"
+    #                        " email3, phone2 from addressbook where deprecated='0000-00-00 00:00:00'")
+    #         for row in cursor:
+    #             (id, firstname, lastname, nickname, address, home, mobile, work, email, email2, email3, phone2) = row
+    #             list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, nickname=nickname,
+    #                                 address=address, homephone=home, mobilephone=mobile, workphone=work,
+    #                                 email=email, email2=email2, email3=email3, secondaryphone=phone2))
+    #     finally:
+    #         cursor.close()
+    #     return list
 
     def destroy(self):
         self.connection.close()
