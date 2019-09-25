746667778625f2c03ae28e1119e0f4eda5c98020
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-25 18:20:09 +0300

    Задание 21 - v2

--- test/test_contacts_info.py
+++ test/test_contacts_info.py
@@ -10,34 +10,34 @@ def test_contact_info_on_home_page_by_db(app, db):
 
 
 # Задание 14
-def test_contact_info_on_home_page_by_app(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
+# def test_contact_info_on_home_page_by_app(app):
+#     contact_from_home_page = app.contact.get_contact_list()[0]
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+#     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+#     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+#     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+#     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+#     assert contact_from_home_page.address == contact_from_edit_page.address
 
 
-def clear(s):
-    return re.sub("[() -]", "", s)
-
-
-def merge_phones_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.homephone, contact.workphone,
-                                        contact.mobilephone, contact.secondaryphone]))))
-
-
-def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.email, contact.email2,
-                                        contact.email3]))))
+# def clear(s):
+#     return re.sub("[() -]", "", s)
+#
+#
+# def merge_phones_like_on_home_page(contact):
+#     return "\n".join(filter(lambda x: x != "",
+#                             map(lambda x: clear(x),
+#                                 filter(lambda x: x is not None,
+#                                        [contact.homephone, contact.workphone,
+#                                         contact.mobilephone, contact.secondaryphone]))))
+#
+#
+# def merge_emails_like_on_home_page(contact):
+#     return "\n".join(filter(lambda x: x != "",
+#                             map(lambda x: clear(x),
+#                                 filter(lambda x: x is not None,
+#                                        [contact.email, contact.email2,
+#                                         contact.email3]))))
 
 
 # def test_phones_on_contact_view_page(app):
