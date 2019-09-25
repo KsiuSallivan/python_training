c497051c18e98da04416c6e8403b767d39f6327a
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-25 16:28:54 +0300

    Отключаемые проверки пользовательского интерфейса - v2

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -16,6 +16,7 @@ def test_modify_contact(app, db, check_ui):
     new_contacts = db.get_contact_list()
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
-        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
+                                                                     key=Contact.id_or_max)
 
 
