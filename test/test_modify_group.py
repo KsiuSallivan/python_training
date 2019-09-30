6717603f9a788a8a0bc614d63f4d3b8291aaba57
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-30 14:00:17 +0300

    Задание 20

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,15 +2,18 @@ from model.group import Group
 import random
 
 
-def test_modify_first_name(app, db, check_ui):
+def test_modify_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create_group(Group(name="147", header="555", footer="666"))
     old_groups = db.get_group_list()
     group = random.choice(old_groups)
     group_fill = Group(name="New funny name")
     app.group.modify_group_by_id(group.id, group_fill)
+    assert len(old_groups) == app.group.count()
     new_groups = db.get_group_list()
+    old_groups[int(group.id)] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
 
