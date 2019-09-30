43b37b8b4737edd807abf9fbb98568272762aec0
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-09-30 11:52:35 +0300

    Задание 14

--- test/test_contacts_info.py
+++ test/test_contacts_info.py
@@ -3,47 +3,38 @@ from model.contact import Contact
 
 
 # Задание 21
-def test_contact_info_on_home_page_by_db(app, db):
-    contact_from_home_page = app.contact.get_contact_list()
-    contact_from_db = db.get_contact_list()
-    assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
+# def test_contact_info_on_home_page_by_db(app, db):
+#     contact_from_home_page = app.contact.get_contact_list()
+#     contact_from_db = db.get_contact_list()
+#     assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
 
 
 # Задание 14
-# def test_contact_info_on_home_page_by_app(app):
-#     contact_from_home_page = app.contact.get_contact_list()[0]
-#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-#     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-#     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-#     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-#     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-#     assert contact_from_home_page.address == contact_from_edit_page.address
+def test_contact_info_on_home_page_by_app(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.address == contact_from_edit_page.address
 
 
-# def clear(s):
-#     return re.sub("[() -]", "", s)
-#
-#
-# def merge_phones_like_on_home_page(contact):
-#     return "\n".join(filter(lambda x: x != "",
-#                             map(lambda x: clear(x),
-#                                 filter(lambda x: x is not None,
-#                                        [contact.homephone, contact.workphone,
-#                                         contact.mobilephone, contact.secondaryphone]))))
-#
-#
-# def merge_emails_like_on_home_page(contact):
-#     return "\n".join(filter(lambda x: x != "",
-#                             map(lambda x: clear(x),
-#                                 filter(lambda x: x is not None,
-#                                        [contact.email, contact.email2,
-#                                         contact.email3]))))
+def clear(s):
+    return re.sub("[() -]", "", s)
 
 
-# def test_phones_on_contact_view_page(app):
-#     contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
-#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-#     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-#     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-#     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
-#     assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.homephone, contact.mobilephone, contact.workphone,
+                                        contact.secondaryphone]))))
+
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.email, contact.email2,
+                                        contact.email3]))))
