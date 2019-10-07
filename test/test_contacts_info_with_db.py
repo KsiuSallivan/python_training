f0e8c096237441e8e93e8dcb2af94b510fe773f4
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-10-07 11:46:30 +0300

    Задание 21 - Попытка 4

--- test/test_contacts_info_with_db.py
+++ test/test_contacts_info_with_db.py
@@ -4,18 +4,18 @@ import re
 
 # Задание 21
 def test_contact_info_on_home_page_by_db(app, db):
-    contact_from_home_page = app.contact.get_contact_list()
-    contact_from_db = db.get_contact_list()
-    # assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
-    # assert contact_from_home_page.all_phones_from_home_page == contact_from_db.all_phones_from_home_page
-    # assert contact_from_home_page.all_emails_from_home_page == contact_from_db.all_emails_from_home_page
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_db = db.get_contact_list()[0]
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_db)
     assert contact_from_home_page.firstname == contact_from_db.firstname
     assert contact_from_home_page.lastname == contact_from_db.lastname
     assert contact_from_home_page.address == contact_from_db.address
+    # assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
 
 
 def twoinone(s):
-    return re.sub('\s+', '', s)
+    return re.sub('\\s+', '', s)
 
 
 def clear(s):
@@ -23,17 +23,18 @@ def clear(s):
     return re.sub("[() -]", "", s)
 
 
-def merge_phones_like_on_home_page(contact):
+def merge_phones_like_on_home_page(contact_from_db):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.homephone, contact.mobilephone, contact.workphone,
-                                        contact.secondaryphone]))))
+                                       [contact_from_db.homephone, contact_from_db.mobilephone, contact_from_db.workphone,
+                                        contact_from_db.secondaryphone]))))
 
 
-def merge_emails_like_on_home_page(contact):
+def merge_emails_like_on_home_page(contact_from_db):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.email, contact.email2,
-                                        contact.email3]))))
+                                       [contact_from_db.email, contact_from_db.email2,
+                                        contact_from_db.email3]))))
+
