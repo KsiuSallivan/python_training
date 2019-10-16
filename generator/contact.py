59020ba8ec121fc2a7b06d5064f4aec9eea7a5eb
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2019-10-16 15:43:30 +0300

    Задание 21 - убрать пробелы из email

--- generator/contact.py
+++ generator/contact.py
@@ -25,17 +25,22 @@ for o, a in opts:
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
+
+
+def random_string_for_email(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + "@._-"
     return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
 
 
 testdata = [Contact(firstname="", lastname="", nickname="", email="", homephone="", mobilephone="", workphone="",
                     secondaryphone="", email2="", email3="", address="", )] + [
     Contact(firstname=random_string("Kseniya", 10), lastname=random_string("Kurashova", 10),
-            nickname=random_string("Kotik", 10), email=random_string("kot@kot.kot", 10),
+            nickname=random_string("Kotik", 10), email=random_string_for_email("kot@kot.kot", 5),
             homephone=random_string("123", 10), mobilephone=random_string("456", 10),
             workphone=random_string("789", 10), secondaryphone=random_string("147", 10),
-            email2=random_string("kot2@kot2.kot", 10), email3=random_string("kot3@kot3.kot", 10),
+            email2=random_string_for_email("kot2@kot2.kot", 5), email3=random_string_for_email("kot3@kot3.kot", 5),
             address=random_string("Gh12", 10))
     for i in range(3)
 ]
